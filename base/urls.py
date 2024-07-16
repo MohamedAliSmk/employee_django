f12fc7d69694d67e0354ac6c8e81a3c01cd2024e
ali alaraby <asaadawy@arabdt.com>
2024-07-16 11:59:56 +0300

    feat: added manual view to add emps attendances

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ from django.views.i18n import set_language
 urlpatterns = [
     path('i18n/', set_language, name = 'set_language'),
     path('', views.home, name = 'home'),
+    path('task', views.task, name = 'task'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .tasks import add_attendance_records
 
 # Create your views here.
 def home(request):
     return render(request, 'base/home.html')
+
+
+def task(request):
+    add_attendance_records()
+    return HttpResponse('Done')
